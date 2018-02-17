10fe7883f436f907ee1ffd232238d1c3d138449b
Vester-David <35358632+Vester-David@users.noreply.github.com>
2018-02-17 10:33:50 -0700

    fixed topic 4 to pull from API and parse to HTML

--- topic4.js
+++ topic4.js
@@ -2,13 +2,19 @@
 function example1() {
 	var myRequest = new XMLHttpRequest();
 	myRequest.onreadystatechange = function() {
-		if (this.status == 200) {
+		// must use readystate AND status. Just using status throws errors when parsing JSON
+		if (this.readyState == 4 && this.status == 200) {
+			// output raw JSON response
 			document.getElementById("example1").innerHTML = this.responseText;
-		}
+			// parse JSON string to Javascript object
+			var myData = JSON.parse(this.responseText);
+			// output Javascript object to HTML
+			document.getElementById("example2").innerHTML = 'Title: <strong>' + myData.title + 
+									'</strong><br><a href=' + myData.url + '><img src=' + 
+									myData.thumbnailUrl + '/><a/><br>Click on Thumbnail to See Full Image';
 		
+		}	
 	};
-	myRequest.open("GET", "loremipsum.txt", true);
+	myRequest.open("GET", "https://jsonplaceholder.typicode.com/photos/3", true);
 	myRequest.send();
-
 }
-
